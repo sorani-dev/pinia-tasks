57854c268b73be81e8063699c9c1e1f9f67417f3
Simon Cateau <sorani.dev@outlook.com>
2023-01-23 15:20:29 +0100

    feat: Install pinia package and make a store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-import 'material-icons/iconfont/material-icons.css';
+import 'material-icons/iconfont/material-icons.css'
 import './assets/main.css'
+import './assets/fonts/Poppins/stylesheet.css'
+import './assets/fonts/VT323/stylesheet.css'
+import {createPinia} from "pinia";
+import {useTaskStore} from "@/store/TaskStore";
 
-createApp(App).mount('#app')
+createApp(App).use(createPinia(useTaskStore)).mount('#app')
