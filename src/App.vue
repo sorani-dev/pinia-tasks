78e2e0305bdf41d7456d712ab2503b9a36aa6ff3
Simon Cateau <sorani.dev@outlook.com>
2023-01-23 15:19:36 +0100

    Initial commit - Project Setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <!-- Heading -->
+    <header>
+      <img src="./assets/logo.svg" alt="Pinia Logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'material-icons/iconfont/material-icons.css';
+import './assets/main.css'
+
+createApp(App).mount('#app')
