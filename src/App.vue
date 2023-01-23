3d4269a0c9ff88cfdfb7c3d3ada48f3ffae664c1
Simon Cateau <sorani.dev@outlook.com>
2023-01-23 16:56:39 +0100

    feat: Add access to the store state, show task details

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,60 @@
       <img src="./assets/logo.svg" alt="Pinia Logo">
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- Task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" :key="task.id" />
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {}
+import {useTaskStore} from "@/store/TaskStore";
+import TaskDetails from "@/components/TaskDetails.vue";
+
+export default {
+  components: {TaskDetails},
+  setup() {
+    const taskStore = useTaskStore()
+
+    return {taskStore}
+  }
+}
+
 </script>
 
 <style scoped>
 
+/* Header */
+header {
+  text-align: center;
+  background: #e7e7e7;
+  padding-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header img {
+  max-width: 60px;
+  transform: rotate(-10deg);
+}
+
+header h1 {
+  margin: 0;
+  font-size: 2em;
+  padding-top: 25px;
+  margin-left: 15px;
+  color: #777;
+  transform: rotate(2deg);
+}
+
+/* Task list*/
+.task-list {
+  max-width: 640px;
+  margin: 20px auto;
+}
 </style>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons">delete</i>
+      <i class="material-icons">favorite</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskDetails",
+  props: ['task']
+}
+</script>
+
+<style scoped>
+
+/* Task */
+.task {
+  padding: 6px 20px;
+  background: #fff;
+  margin-top: 20px;
+  border-radius: 4px;
+  box-shadow: 2px 4px 6px rgba(0, 0, 0, .05);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.task h3, .task .icons {
+  display: inline-block;
+}
+
+.task .icons {
+  text-align: right;
+}
+.task i {
+  font-size: 1.4em;
+  margin-left: 6px;
+  cursor: pointer;
+  color: #bbb;
+}
+</style>
