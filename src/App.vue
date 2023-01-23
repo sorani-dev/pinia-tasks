665f5cfff434d6a337a4e4cb674f4c82a2533f40
Simon Cateau <sorani.dev@outlook.com>
2023-01-23 17:43:27 +0100

    feat: Add getters for all tasks and favorites

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,25 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+
+    <!-- Filter bar   -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favorites'">Favorite tasks</button>
+    </nav>
+
     <!-- Task list -->
-    <div class="task-list">
+    <div class="task-list" v-if="filter === 'all'">
+      <p>Your have {{ taskStore.totalCount }} task{{ plural }} left to do</p>
       <div v-for="task in taskStore.tasks" :key="task.id">
+        <TaskDetails :task="task" :key="task.id" />
+      </div>
+    </div>
+
+    <!-- Favorites task list -->
+    <div class="task-list" v-if="filter === 'favorites'">
+      <p>Your have {{ taskStore.favoritesCount }} favorite{{ plural }} left to do</p>
+      <div v-for="task in taskStore.favorites" :key="task.id">
         <TaskDetails :task="task" :key="task.id" />
       </div>
     </div>
@@ -16,22 +32,26 @@
 </template>
 
 <script>
-import {useTaskStore} from "@/store/TaskStore";
+import { ref } from "vue";
+
+import { useTaskStore } from "@/store/TaskStore";
 import TaskDetails from "@/components/TaskDetails.vue";
 
 export default {
-  components: {TaskDetails},
+  components: { TaskDetails },
   setup() {
     const taskStore = useTaskStore()
 
-    return {taskStore}
+    const filter = ref('all')
+    const plural = taskStore.tasks.length > 1 ? 's' : ''
+
+    return { filter, taskStore, plural }
   }
 }
 
 </script>
 
 <style scoped>
-
 /* Header */
 header {
   text-align: center;
@@ -60,5 +80,25 @@ header h1 {
 .task-list {
   max-width: 640px;
   margin: 20px auto;
+}
+
+/* Filter Navigation */
+.filter {
+  max-width: 640px;
+  /*width: 640px;*/
+  margin: 10px auto;
+  text-align: right;
+}
+
+.filter button {
+  display: inline-block;
+  margin-left: 10px;
+  background: #fff;
+  border: 1px solid #555;
+  border-radius: 4px;
+  padding: 4px 8px;
+  cursor: pointer;
+  font-size: .8em;
+  font-size: 1em;
 }
 </style>
--- src/store/TaskStore.js
+++ src/store/TaskStore.js
@@ -6,6 +6,18 @@ export const useTaskStore = defineStore('taskStore',
             tasks: [
                 {id: 1, title: "buy some milk", isFav: false},
                 {id: 2, title: "play Sauerbraten", isFav: true},],
-            name: 'Simon'
-        })
+        }),
+        getters: {
+            favorites() {
+                return this.tasks.filter(t => t.isFav)
+            },
+            favoritesCount() {
+                return this.tasks.reduce((p, current) => {
+                    return current.isFav ? p+1 : p
+                }, 0)
+            },
+            totalCount(state) {
+                return state.tasks.length
+            }
+        }
     })
